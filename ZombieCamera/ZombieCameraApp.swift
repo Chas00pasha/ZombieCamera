477a57a9025665c43e25a4cd8e7c9e818107621e
Chas00pasha <PChasovnikov@gmail.com>
2026-06-30 14:25:30 +0100

    Initial Commit

---
+++ ZombieCamera/ZombieCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZombieCameraApp.swift
+//  ZombieCamera
+//
+//  Created by Mac on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct ZombieCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
